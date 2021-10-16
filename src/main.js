e33b9cee8b06ed34e0cdc300aa5e78fb2df3f7c9
Constance <hytan.2018@sis.smu.edu.sg>
2021-10-16 21:51:38 +0800

    add bootstrap dependencies

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
 Vue.config.productionTip = false
 
 new Vue({
