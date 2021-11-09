f69c2d6cc95425a46678f4aa67cc82616411d388
h-ejia <92679281+h-ejia@users.noreply.github.com>
2021-11-09 21:53:47 +0800

    Update Artist.vue

--- src/views/Artist.vue
+++ src/views/Artist.vue
@@ -2,31 +2,30 @@
     <!-- documentation: https://github.com/ankurk91/vue-loading-overlay -->
 <div>
   <div><li v-for="item in dataItems" :key="item">{{ item.name }}</li></div>
+  <div><li v-for="item1 in artistData" :key="item1">{{ item1 }}</li></div>
+
   <div class="album">
-
-   
-
         <loading
         :active="dataLoading"
         :background-color="theme == 'light' ? 'white' : 'black'"
         color="green"
         loader="bars"
         />
-         <div class="container">
 
 
+        <div class="container">
         <div class="row">
-            <!--Empty Col-->
-            <div class="col-xl-1 col-lg-1 col-md-12 ">
-            </div>
+
             <!--Album Picture-->
-            <div class="col-xl-4 col-lg-5 col-md-12" >
+            <div class="col-xl-5 col-lg-12" >
                 <img class="img1 rounded center-block" src="https://www.esplanade.com/-/media/offstage-microsite/know-the-artists/now-hear-this-charlie-lim/nht_charlie_lim_1000x1000.jpg?mw=640" alt="Card image">
             </div>
             <!--Album Writeup + Info-->
-            <div class="writeup col-xl-6 col-lg-5 col-md-12 ">
+            <div class="writeup col-xl-7 col-lg-12 ">
                 <h1>Charlie Lim</h1>
                 <h4>Artist</h4>
+    <iframe :src="'https://open.spotify.com/follow/1/?uri=spotify:artist:'+artistId+'&size=basic&theme='+theme"
+            width="150" height="27" scrolling="no" frameborder="0" allowtransparency="true"/>
 
                 <p style="color:white ;">Classically trained pianist Charlie Lim had an early start in music, sowing the seeds of ambition in church choirs and school bands since he was 14 years old. 2011’s self-titled debut EP kickstarted his journey into songwriting, blending neo-soul, electronic pop, R&B, folk and velvety vocals into a bright, breezy and groove-laden sound. <span id="dots">...</span><span id="more"> Released in 2015, Lim’s breakout double EP TIME / SPACE swept the charts and soundtracked festivals that year with its head-bobbing, electronica-tinged pop that fluently evoked a mix of moods. Lim continues to be an inspirational force on the Southeast Asian music scene, boldly pushing his musicality into exciting new places like 2018’s fresh-sounding effort, CHECK-HOOK, which slaloms between R&B and smart electronics. Lim draws lyrical inspiration from the reality of daily life, allowing a younger generation of music fans to find affinity in his universal observations.</span></p>
                 
@@ -38,17 +37,11 @@
             <router-link :to="'/chat/artist:'+artistName"><Button class="btn btn-success" id="chatButton" v-tooltip="'Chat with others about this artist'">Connect with fans</Button></router-link><br>
                 </div> 
             </div>
+     
 
-            <!--Empty Col-->
-            <div class="col-xl-1 col-lg-1 col-md-12 ">
-            </div>
             
         </div>
-
-
-    
-         </div>
-
+        </div>
 
         <!--Other Albums-->
 
@@ -106,27 +99,28 @@ export default {
   props: {
   //artistId: "3FodFdWfVWIiER6Cv6YVVQ"
   },
-  data() {
-    return {
-      /* can customise */
-      artistId: "3FodFdWfVWIiER6Cv6YVVQ",
-      dataLimit: 12,
-      /* will be updated automatically */
-      dataOffset: 0,
-      dataLoading: true,
-      dataItems: [],
-      dataPages: 0,
-      dataActivePage: 1,
-      /* temporary fallbacks */
-      dataArtistIds: [],
-      artistName: 'name',
-      activeAlbum: "5Ay88ZVN61blW8QYUpofy6",
-    }
-  },
-  mounted() {
+    data() {
+        return {
+        /* can customise */
+        artistId: "3FodFdWfVWIiER6Cv6YVVQ",
+        dataLimit: 12,
+        /* will be updated automatically */
+        dataOffset: 0,
+        dataLoading: true,
+        dataItems: [],
+        artistData: [],
+        dataPages: 0,
+        dataActivePage: 1,
+        /* temporary fallbacks */
+        dataArtistIds: [],
+        artistName: 'name',
+        activeAlbum: "5Ay88ZVN61blW8QYUpofy6",
+        }
+    },
+    mounted() {
           setTimeout(() => console.log(this.$refs.test.dataItems), 900)
-        
-  },
+          setTimeout(() => console.log(this.$refs.test.artistData), 900)
+    },
     methods: {
       readMoreLess: function() {
             console.log("test");
@@ -144,12 +138,12 @@ export default {
                 moreText.style.display = "inline";
             }
         },
-      getArtistAlbums() {
+    getArtistAlbums() {
       /* documentation: https://jmperezperez.com/spotify-web-api-js/#src-spotify-web-api.js-constr.prototype.getartistalbums */
       SpotifyApi
         .getArtistAlbums(this.artistId, { limit: this.dataLimit, offset: this.dataOffset })
         .then((data) => {
-          console.log(data)
+          //console.log(data)
           this.dataItems = data.items
           this.dataPages = Math.ceil(data.total / this.dataLimit)
           //this.dataArtistIds = 
@@ -166,16 +160,39 @@ export default {
           this.dataLoading = false
           this.$refs["errorArtistAlbums"].innerText = "Error occurred. Please try again."
         })
-    },
+        },
+    getArtist() {
+      /* documentation: https://jmperezperez.com/spotify-web-api-js/#src-spotify-web-api.js-constr.prototype.getartistalbums */
+      SpotifyApi
+        .getArtist(this.artistId, { limit: this.dataLimit, offset: this.dataOffset })
+        .then((data) => {
+          this.artistData = data.items
+          console.log(data.items)
+          this.dataLoading = false
+          this.$refs["errorArtistAlbums"].innerText = ""
+
+          /* temporary info for widgets */
+          this.artistName = this.artistData[0].artists[0].name
+          this.activeAlbum = this.artistData[0].id
+        })
+        .catch((error) => {
+          // console.log(error.responseText)
+          this.dataLoading = false
+          this.$refs["errorArtistAlbums"].innerText = "Error occurred. Please try again."
+        })
+        },
     handlePaginate(page) {
       this.dataOffset = (page - 1) * this.dataLimit
       this.getArtistAlbums()
+      this.getArtist()
       this.dataActivePage = page
     }
   },
     created() {
     /* give time to set access token in spotify-auth.js */
     setTimeout(() => this.getArtistAlbums(), 800)
+    setTimeout(() => this.getArtist(), 800)
+
   },
     computed: {
     theme() {
@@ -213,8 +230,8 @@ export default {
         }
         .writeup{
             padding-top: 10px;
-            padding-left: 60px;
-            padding-right: 60px;
+            padding-left: 45px;
+            padding-right: 45px;
         }
 
         .main {
