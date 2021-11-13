a350d68167cde8a8a7baf4af184a81c76f017faa
h-ejia <92679281+h-ejia@users.noreply.github.com>
2021-11-13 17:45:46 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,134 +1,280 @@
 <template>
-  <div class="container login p-4">
+<div>
+  <!-- documentation: https://github.com/ankurk91/vue-loading-overlay -->
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container-text">
     <div class="row">
-      <div class="col-lg-3 col-md-2"></div>
-      <div class="col-lg-6 col-md-8 login-box">
-        <div class="col-lg-12 login-box_title">KayaJam</div>
-        <div class="col-lg-12 login-box_form">
-          <div class="form-group">
-            <label for="givenUsername">Username</label>
-            <input type="text" class="form-control" id="givenUsername" v-model="givenUsername" placeholder="Enter Name" autocomplete="off">
-          </div>
-          <div class="form-group">
-            <label for="givenPwd">Password</label>
-            <input type="password" class="form-control" id="givenPwd" v-model="givenPwd" placeholder="Password" @keyup.enter="validate">
-          </div>
-          <Button @click.native="validate()" btn-class="btn-primary">Submit</Button>
-          <div class="alert" v-if="showError">{{ error }}</div>
-          <div v-else>{{ redirect() }}</div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-2"></div>
+        <h1 class="display-4">Welcome to the great Singapore jam!</h1>
+        <h3>Discover local artists and connect with fellow music lovers</h3>
     </div>
+    <div class="row mt-3">
+        <a href="./music">
+        <button type="button" class="btn text-dark btn-light">Fall in love with 🇸🇬 music</button>
+        </a>
+    </div>
+    <div class="row">
+    </div>
+</div>
   </div>
+
+  <div class="container-fluid px-4">
+	<div class="row gx-5 mt-4">
+
+		<div class="learn col-md-12 text-align-centre">
+				<h4>This week's featured artists</h4>
+			</div>
+		<div class="col-md-4 text-align-centre">	
+			<a href="./#/artist/3FodFdWfVWIiER6Cv6YVVQ">
+			<div class="story-col bg-light text-dark ">
+				<img class="img1" src="https://i.scdn.co/image/ab6761610000e5ebaff746d063c48fa0dcb8fdf8" alt="Card image"/>
+				<h4 class="mt-3">Charlie Lim</h4>
+			</div>
+			</a>
+		</div>
+		<div class="col-md-4 text-align-centre">
+			<a href="./#/artist/5IIP34JBy1d8kBYlAGnRaW">
+			<div class="story-col bg-light text-dark ">
+				<img class="img1" src="https://i.scdn.co/image/ab6761610000e5eb03ea148d3c0019738ea77bb4" alt="Card image"/>
+				<h4 class="mt-3">Linying</h4>
+			</div>
+			</a>
+
+		</div>
+		<div class="col-md-4 text-align-centre">
+			<a href="./#/artist/2HXfSr5CfTPZbcqS2gyGYm">
+			<div class="story-col bg-light text-dark ">
+				<img class="img1" src="https://i.scdn.co/image/ab6761610000e5eb339ad9527609417a8c66bd88" alt="Card image"/>
+				<h4 class="mt-3">Yung Raja</h4>
+			</div>
+			</a>
+		</div>
+
+
+
+
+
+		<div class="learn col-md-12 text-align-centre">
+			<h4>Learn more about us</h4>
+		</div>
+		<div class="col-md-6">
+		<div class="info-col bg-light text-dark">
+		<h4 >Our Story</h4>
+		<p class="text-justify">We’re a collective of Singaporean artists passionate about local arts and music. With fellow musicians’ livelihoods being affected by the Coronavirus pandemic, we wanted to create a platform that actively promotes and empowers local artists. Join our community and connect with your fellow music lovers and creators.</p>
+		</div>
+		</div>
+		<div class="col-md-6">
+		<div class="info-col bg-success text-white">
+		<h4>Our Mission</h4>
+		<p class="text-justify">KayaJam interfaces with the entire cross section of Singapore's music industry and we see our role as builders of the ecosystem. From our work, we desire to see artists in Singapore flourishing, greater proliferation of ideas that enrich humanity, and more interest in local music.</p>
+		<p></p>
+		</div>
+		</div>
+	</div>
+  </div>
+
+</div>
 </template>
 
 <script>
-import Button from '@/components/Btn.vue'
+import Star from '@/components/Star.vue'
+import Button from "@/components/Btn.vue";
+import ButtonSocialShare from "@/components/BtnSocialShare.vue";
+import Loading from "vue-loading-overlay";
+import { toastedOptions } from '@/utils'
+import { mapState, mapMutations } from 'vuex'
+import SpotifyApi from "@/services/spotify-auth";
+
 
 export default {
-  name: "Login",
-  components: {
-    Button
-  },
-  /* adapted from: SMU IS216 extra exercise */
-  data() {
-    return {
-      users: [
-        { username: "Jack", pwd: "abc" },
-        { username: "Mary", pwd: "def" },
-        { username: "John", pwd: "123" },
-        { username: "Cherry", pwd: "456" },
-      ],
-      givenUsername: "",
-      givenPwd: "",
+	name: "Artist",
+	components: {
+		//Button,
+    //ButtonSocialShare,
+		//Loading,
+	},
+	data() {
+		return {
+			/* can customise */
+			dataLimit: 3,
+			artistID: "3FodFdWfVWIiER6Cv6YVVQ",
+			albumID: "64O0VSauCmZ31AljgallQ4",
+			/* will be updated automatically */
+			dataOffset: 0,
+			dataLoading: true,
+			dataPages: 0,
+			dataActivePage: 1,
+			albumDataItems: [],
+			artistData: {images:[{url:''}]},	/* prevent error when haven't set */
+		};
+	},
+	methods: {
+	getAlbum() {
+		/* documentation: https://jmperezperez.com/spotify-web-api-js/#src-spotify-web-api.js-constr.prototype.getartistalbums */
+		SpotifyApi.getAlbum(this.albumID, { limit: this.dataLimit, offset: this.dataOffset })
+			.then((data) => {
+				this.dataLoading = false;
+				this.albumData = data;
+				console.log(this.albumData)
+				this.dataPages = Math.ceil(data.total / this.dataLimit);
+				})
+			.catch((error) => {
+				this.dataLoading = false;
+				// console.log(error.responseText)
+				this.$toasted.error("Error occurred while fetching data. Please try again.", toastedOptions)
+				this.$toasted.info(`Feel free to contact us for any inquiries at ${process.env.VUE_APP_EMAIL} `, toastedOptions)
+			});
+	},
+	getArtist() {
+		/* documentation: https://jmperezperez.com/spotify-web-api-js/#src-spotify-web-api.js-constr.prototype.getartistalbums */
+		SpotifyApi.getArtist(this.albumID, { limit: this.dataLimit, offset: this.dataOffset })
+			.then((data) => {
+				this.dataLoading = false;
+				this.albumData = data;
+				console.log(this.albumData)
+				this.dataPages = Math.ceil(data.total / this.dataLimit);
+				})
+			.catch((error) => {
+				this.dataLoading = false;
+				// console.log(error.responseText)
+				this.$toasted.error("Error occurred while fetching data. Please try again.", toastedOptions)
+				this.$toasted.info(`Feel free to contact us for any inquiries at ${process.env.VUE_APP_EMAIL} `, toastedOptions)
+			});
+	},
+     ...mapMutations(['setPlayerAlbum']),
 
-      error: "",
-      showError: true,
-    };
-  },
-  methods: {
-    validate() {
-      // clear previous error
-      this.error= "";
-      this.showError = false;
+    getRandomAlbum() {
+      return this.artistDataItems[Math.floor(Math.random() * this.dataLimit)]
+    },
+		storeArrayAsString: function (genres) {
+      if (!genres) return ""
+			var str = "";
+			for (var i = 0; i < genres.length; i++) {
+				str += this.capitaliseFirstLetter(genres[i]) + ", ";
+			}
+			if (genres.length == 1) {
+				str = "<i>Genre:</i> " + str.substring(0, str.length - 2);
+			} else {
+				str = "<i>Genres:</i> " + str.substring(0, str.length - 2);
+			}
+			return str;
+		},
+		capitaliseFirstLetter: function (passedstring) {
+      if (!passedstring) return ""
+			//This function is used for Type and Genres
+			const arr = passedstring.split(" ");
+			for (var i = 0; i < arr.length; i++) {
+				arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+			}
+			return arr.join(" ");
+		},
+		// readMoreLess: function () {
+		// 	//This function is used for the Read More/Less Button
+		// 	//console.log("test");
+		// 	var dots = document.getElementById("dots");
+		// 	var moreText = document.getElementById("more");
+		// 	var btnText = document.getElementById("readmoreButton");
 
-      if (this.givenUsername == "" || this.givenPwd == "") {
-        // check if any of the input fields are empty
-        this.error = "Username and password cannot be empty";
-        this.showError = true;
-      } else {
-        // check if username exists in the data
-        let userExist = false;
-        let pwd = "";
-        for (let a_user of this.users) {
-          if (a_user.username == this.givenUsername) {
-            userExist = true;
-            pwd = a_user.pwd;
-            break;
-          }
-        }
-        if (!userExist) {
-          this.error = "Username does not exist!";
-          this.showError = true;
-        } else if (pwd != this.givenPwd) {
-          this.error = "Password incorrect!";
-          this.showError = true;
-        }
-      }
-    },
-    redirect() {
-      this.$router.push("/");
-    },
-  },
+		// 	if (dots.style.display === "none") {
+		// 		dots.style.display = "inline";
+		// 		btnText.innerHTML = "Read more";
+		// 		moreText.style.display = "none";
+		// 	} else {
+		// 		dots.style.display = "none";
+		// 		btnText.innerHTML = "Read less";
+		// 		moreText.style.display = "inline";
+		// 	}
+    // },
+
+   
+		handlePaginate(page) {
+			this.dataOffset = (page - 1) * this.dataLimit;
+			this.getAlbum();
+			this.getArtist();
+			this.dataActivePage = page;
+		},
+	},
+	created() {
+		/* give time to set access token in spotify-auth.js */
+		setTimeout(() => {
+			this.getAlbum()
+			this.getArtist()
+    }, 800)
+	},
+	computed: {
+		...mapState(["theme"]),
+		id() {
+      return this.$route.params.id;
+    }
+	},
 };
 </script>
 
 <style scoped lang="scss">
-.login-box {
-  height: fit-content;
-  max-width: 550px;
-  margin-bottom: 35px;
-  margin-top: 35px;
-  padding-right: 45px;
-  padding-left: 45px;
+.container-text{
   text-align: center;
-  background: map-get($colors-brands, spotify);
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  color: black;
-
-  &_title {
-    margin-top: 30px;
-    font-size: 50px;
-    letter-spacing: 2px;
-    font-weight: bold;
-  }
-
-  &_form {
-    margin-top: 25px;
-    text-align: left;
-  }
+  vertical-align: middle;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+  color: white;
 }
 
-.form-group {
-  margin-bottom: 40px;
+.jumbotron{
+  background-image: url(https://freight.cargo.site/t/original/i/3f1e5387264d1dd8f60951413c7df3675b74366163cb27386f48dc70ed664925/418A8597-S.jpg);
+  background-size: 100% auto;
+  background-position: center center;
+  height: 300px;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  vertical-align: middle;
+  align-items: center;
+}
+.learn{
+  padding-left: 30px;
+  padding-right: 30px;
+  margin: 10px;
+}
+.story-col{
+  min-height: 260px;
+  border-radius: 10px;
+  padding: 30px;
+  margin: 10px;
+  margin-bottom: 30px;
+box-shadow: 0 6px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.05);
+  transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),.3s box-shadow,.3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);
+}
 
-  .form-control {
-    margin-bottom: 20px;
-    padding-left: 5px;
-    border: none;
-    font-weight: bold;
-    background-color: black;
-    color: white;
+.story-col:hover{
+     transform: scale(1.05);
+  box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);
+}
 
-    &:focus {
-      outline: none;
-      box-shadow: 0 0 0;
-    }
-  }
+.info-col{
+  min-height: 260px;
+  border-radius: 10px;
+  padding: 30px;
+  margin: 10px;
+  margin-bottom: 30px;
+}
 
-  label {
-    margin-bottom: 0;
-  }
+.btn{
+  max-width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  display: inline-block;
+  border-radius: 50px;
+  font-size: 20px;
+}
+
+.img1 {
+	height: 100%;
+	width: 100%;
+	object-fit: contain;
+	margin: auto;
+	object-position: center;
+	justify-content: center;
 }
 </style>
